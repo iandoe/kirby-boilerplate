e3a4937075b78906582656d0e20e3c650deffb0a
Wilhearts <wilhearts@gmail.com>
2014-02-20 16:19:19 +0100

    Added gulp

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,6 @@
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/assets/js/dist/jquery-2.1.0.min.js"><\/script>')</script>
 
 <?php echo js('assets/js/plugins.min.js') ?>
 <?php echo js('assets/js/main.min.js') ?>
