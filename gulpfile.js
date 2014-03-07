fc54b5f12c481b5edc389895d00018bd33c9c005
Wilhearts <wilhearts@gmail.com>
2014-03-07 10:57:06 +0100

    compass fonts path, gulpfile mods

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp = require("gulp"),
-    plugins = require('gulp-load-plugins')({camelize: true});
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')({camelize: true}),
+    server = require('tiny-lr')();
 
 gulp.task('watch', function() {
-    gulp.watch('assets/sass/**/*.scss', ['css']);
+    gulp.watch('assets/sass/**/*.scss', ['css', 'imagemin']);
     gulp.watch('assets/js/main.js', ['js']);
     gulp.watch('assets/js/libs/*.js', ['jsplugins']);
-    gulp.watch('assets/img/src/*', ['imagemin']);
+    // gulp.watch('assets/img/src/*.png', ['imagemin']);
 });
 
 gulp.task('css', function() {
@@ -24,6 +25,7 @@ gulp.task('css', function() {
         }))
         .pipe(plugins.rename({suffix: '.min'}))
         .pipe(gulp.dest('assets/css'))
+        // .pipe(plugins.livereload(server))
         .pipe(plugins.notify("CSS Succesfully Compiled"))
 });
 
@@ -46,12 +48,14 @@ gulp.task('jsplugins', function() {
 
 gulp.task('imagemin', function() {
     return gulp.src('assets/img/src/*')
-    .pipe(plugins.cache(
+    .pipe(
+        // plugins.cache(
         plugins.imagemin({
             optimizationLevel: 5,
             progressive: false,
             interlaced: true
-        }))
+        })
+        // )
     )
     .pipe(gulp.dest('assets/img'))
     .pipe(plugins.notify("Images Compressed"))
